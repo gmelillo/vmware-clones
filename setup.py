9d2b985bce086f1ff71353a20a6f687a12fc7a4b
Gabriel Melillo <gabriel@melillo.me>
2015-09-01 18:08:11 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,34 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
 ]
 
+REQUIREMENTS = [
+    "argparse==1.2.1",
+    "pyvmomi==5.5.0.2014.1.1",
+    "requests==2.7.0",
+    "six==1.9.0",
+    "wsgiref==0.1.2",
+]
+
+PLATFORMS = [
+    'OS Independent'
+]
+
+PACKAGES = [
+    'vclones',
+]
+
+DATA = [
+    ('vclones', ['requirements.txt']),
+    ('vvclones', ['esxi.ini.sample'])
+]
+
+ENTRY_POINTS = {
+    'console_scripts': [
+        'vmwareclone-batch = vclones:batch',
+        'vmwareclone = vclones:main'
+    ]
+}
+
 setup(
     name='vmware-cloning',
     version='0.1.4',
@@ -20,32 +48,16 @@ setup(
     maintainer_email="gabriel@melillo.me",
     description="Clone running vmware virtual machines running on preconfigured cluster",
     url="https://github.com/gmelillo/esxi-backup",
-    install_requires=[
-        "argparse==1.2.1",
-        "pyvmomi==5.5.0.2014.1.1",
-        "requests==2.7.0",
-        "six==1.9.0",
-        "wsgiref==0.1.2",
-    ],
+    install_requires=REQUIREMENTS,
     classifiers=CLASSIFIERS,
-    platforms=['OS Independent'],
-    data_files=[
-        ('vclones', ['requirements.txt']),
-        ('vvclones', ['esxi.ini.sample'])
-    ],
+    platforms=PLATFORMS,
+    data_files=DATA,
     include_package_data=True,
-    packages=[
-        'vclones',
-    ],
+    packages=PACKAGES,
     license="GNU GENERAL PUBLIC LICENSE",
     long_description='Clone all running virtual machines in a running vmware cluster to a specified VCSA'
                      '\n\n',
-    entry_points={
-        'console_scripts': [
-            'vmwareclone-batch = vclones:batch',
-            'vmwareclone = vclones:main'
-        ]
-    }
+    entry_points=ENTRY_POINTS
 )
 
 
