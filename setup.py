3debe575ffefb0baac64990f28c162ce06b32475
Gabriel Melillo <gabriel@melillo.me>
2015-09-01 12:59:16 +0200

    Prepared setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+__author__ = 'gabriel'
+
+from distutils.core import setup
+from pip.req import parse_requirements
+from setuptools.command.develop import develop
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Information Technology',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='vmware-cloning',
+    version='0.1.2',
+    author="Gabriel Melillo",
+    author_email="gabriel@melillo.me",
+    maintainer="Gabriel Melillo",
+    maintainer_email="gabriel@melillo.me",
+    description="Clone running vmware virtual machines running on preconfigured cluster",
+    url="https://github.com/gmelillo/esxi-backup",
+    install_requires=[
+        "argparse==1.2.1",
+        "pyvmomi==5.5.0.2014.1.1",
+        "requests==2.7.0",
+        "six==1.9.0",
+        "wsgiref==0.1.2",
+    ],
+    classifiers=CLASSIFIERS,
+    platforms=['OS Independent'],
+    data_files=[
+        ('vmware-cloning', ['requirements.txt']),
+        ('vmware-cloning', ['esxi.ini.sample'])
+    ],
+    include_package_data=True,
+    packages=[
+        'vmwarecloning',
+    ],
+    license="GNU GENERAL PUBLIC LICENSE",
+    long_description='Export configuration from ESXi 5.X host and upload it to a central repository'
+                     '\n\n',
+    entry_points={'console_scripts': 'vmwareclone-batch = vcones:batch'}
+)
+
+
