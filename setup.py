e34b9f756ba7b3e650ea51efdc76b6fbf005ab92
Gabriel Melillo <gabriel@melillo.me>
2019-04-15 10:00:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CLASSIFIERS = [
 REQUIREMENTS = [
     "argparse==1.2.1",
     "pyvmomi==5.5.0.2014.1.1",
-    "requests==2.7.0",
+    "requests==2.20.0",
     "six==1.9.0",
     "wsgiref==0.1.2",
     "Jinja2==2.10.1",
